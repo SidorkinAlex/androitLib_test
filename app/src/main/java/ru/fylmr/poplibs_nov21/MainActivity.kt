4f927857f4d8400d76e213c395f5610a66ce916a
Maksimov, Filipp <filipp.maksimov@renpensions.com>
2021-11-23 18:32:08 +0200

    Урок 1: viewbinding

--- app/src/main/java/ru/fylmr/poplibs_nov21/MainActivity.kt
+++ app/src/main/java/ru/fylmr/poplibs_nov21/MainActivity.kt
@@ -1,12 +1,24 @@
 package ru.fylmr.poplibs_nov21
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.fylmr.poplibs_nov21.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private var _binding: ActivityMainBinding? = null
+    private val binding
+        get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        binding.helloWorldTextView.text = getString(R.string.app_name)
     }
 }
