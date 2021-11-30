0895359ebc103fed62f66dbd9e2e53aae70a2db1
SidorkinAlex <palach.orl@mail.ru>
2021-11-30 13:08:08 +0300

    задание для урока 1

---
+++ app/src/main/java/ru/fylmr/poplibs_nov21/Counters.kt
@@ -0,0 +1,7 @@
+package ru.fylmr.poplibs_nov21
+
+data class Counters(
+    var counters3: Int = 0,
+    var counters1: Int = 0,
+    var counters2: Int = 0
+) {}
--- app/src/main/java/ru/fylmr/poplibs_nov21/CountersModel.kt
+++ app/src/main/java/ru/fylmr/poplibs_nov21/CountersModel.kt
@@ -4,18 +4,31 @@ import androidx.annotation.IntRange
 
 class CountersModel {
 
-    private val counters = mutableListOf(0, 0, 0)
+    private val counters = Counters(0,0,0)
 
-    fun getCurrent(@IntRange(from = 0, to = 2) index: Int): Int {
-        return counters[index]
+
+    fun increment1(): Int {
+        counters.counters1 = counters.counters1 + 1
+        return counters.counters1
     }
-
-    fun increment(@IntRange(from = 0, to = 2) index: Int): Int {
-        counters[index]++
-        return getCurrent(index)
+    fun increment2(): Int {
+        counters.counters2 = counters.counters2 + 1
+        return counters.counters2
+    }
+    fun increment3(): Int {
+        counters.counters3 = counters.counters3 + 1
+        return counters.counters3
     }
 
     fun set(index: Int, value: Int) {
-        counters[index] = value
+        if(index == 0){
+            counters.counters1 = value
+        }
+        if(index == 2){
+            counters.counters2 = value
+        }
+        if(index == 3){
+            counters.counters3 = value
+        }
     }
 }
